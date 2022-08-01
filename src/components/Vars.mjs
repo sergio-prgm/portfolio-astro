ab575198168aff610fd9723b01d8d0c365eea4d8
sergio-dev <sergio-dev@outlook.com>
2022-08-01 16:47:27 +0200

    Feat: add about section

---
+++ src/components/Vars.mjs
@@ -0,0 +1,10 @@
+const colors = {
+  clrAccent: '#df347a',
+  clrPrimary: 'hsl(0, 0%, 10%)',
+  clrSecondary: 'hsl(0, 0%, 96%)',
+  clrHighlight: '#df473ab0',
+  fontPrincipal: 'Aktiv Grotesk',
+  fontTitle: 'Serat'
+}
+
+export default colors
