05e893034272ccb506ba14b586f8df61a1d3efbe
sergio-dev <sergio-dev@outlook.com>
2022-08-03 13:19:32 +0200

    Feat: style changes

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -2,17 +2,19 @@ const $ = (item) => window.document.querySelector(item)
 
 const primaryNav = $('#primary-navigation')
 const navToggle = $('#mobile-nav-toggle')
+const body = $('body')
 const sectionAnchors = window.document.querySelectorAll('li>a')
 
 navToggle.addEventListener('click', () => {
   const visibility = primaryNav.getAttribute('data-visible')
 
   if (visibility === 'false') {
+    console.log(body)
     primaryNav.setAttribute('data-visible', 'true')
     navToggle.setAttribute('aria-expanded', true)
+    body.classList.add('disable-scroll')
   } else {
-    primaryNav.setAttribute('data-visible', 'false')
-    navToggle.setAttribute('aria-expanded', false)
+    backToNormal()
   }
 })
 
@@ -20,7 +22,12 @@ sectionAnchors.forEach(a => a.addEventListener('click', () => {
   const visibility = primaryNav.getAttribute('data-visible')
 
   if (visibility !== 'false') {
-    primaryNav.setAttribute('data-visible', 'false')
-    navToggle.setAttribute('aria-expanded', false)
+    backToNormal()
   }
 }))
+
+const backToNormal = () => {
+  primaryNav.setAttribute('data-visible', 'false')
+  navToggle.setAttribute('aria-expanded', false)
+  body.classList.remove('disable-scroll')
+}
